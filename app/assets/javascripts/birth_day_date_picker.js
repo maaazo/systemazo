7a1ebbfd8cc94358bb500c6b44d6d367da23da0b
Jonathan Rutigliano <rutigliano.jonathan@gmail.com>
2024-03-12 20:20:13 -0700

    Fix date that's displayed

--- app/assets/javascripts/birth_day_date_picker.js
+++ app/assets/javascripts/birth_day_date_picker.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Get the date input field
-  var dateOfBirthField = document.getElementById("date-of-birth-field");
+  var dateOfBirthField = document.getElementById("user_date_of_birth");
 
-  // Get the minimum date (18 years ago)
-  var minDate = new Date();
-  minDate.setFullYear(minDate.getFullYear() - 18);
+  // Set the maximum date (1906)
+  var minDate = new Date(1906, 0, 1); // January 1, 1906
 
-  // Convert the minimum date to a string in "yyyy-mm-dd" format
+  // Set the minimum date (2006 - 18 years)
+  var maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() - 18);
+
+  // Convert the maximum and minimum dates to strings in "yyyy-mm-dd" format
   var minDateString = minDate.toISOString().split('T')[0];
+  var maxDateString = maxDate.toISOString().split('T')[0];
 
-  // Set the minimum attribute of the date input field
+  // Set the maximum and minimum attributes of the date input field
   dateOfBirthField.setAttribute("min", minDateString);
+  dateOfBirthField.setAttribute("max", maxDateString);
 });
